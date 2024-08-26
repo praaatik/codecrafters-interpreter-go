3e9f06c481d144dbcb81cd219d3278a2396d2fab
Pratik Kulkarni <pnkulkarni@proton.me>
2024-08-26 19:16:46 +0530

    codecrafters submit [skip ci]

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -1,14 +1,119 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+// Lexeme -> Token
 
+type TokenType int
+
+const (
+	EOF TokenType = iota
+	LEFT_PAREN
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+)
+
+type Token struct {
+	Type    TokenType   // Type would classify each lexeme
+	Lexeme  string      // each word is a lexeme in the code
+	Literal interface{} // literal values for strings and numbers
+	Line    int         // store the line number to get the location information
+}
+
+type Scanner struct {
+	source  []byte // raw code which is being read
+	tokens  []Token
+	start   int // index the start of the string
+	current int // index the current character under examination
+	line    int // track the current line for error handling
+}
+
+func NewScanner(source []byte) *Scanner {
+	return &Scanner{
+		source:  source,
+		tokens:  []Token{},
+		start:   0,
+		current: 0,
+		line:    1,
+	}
+}
+
+func (s *Scanner) AddLiteral(tokenType TokenType, literal interface{}) {
+	tt := string(s.source[s.start:s.current])
+	s.tokens = append(s.tokens, Token{tokenType, tt, literal, s.line})
+}
+
+// AddToken calls the AddLiteral to add the literal type
+func (s *Scanner) AddToken(tokenType TokenType) {
+	s.AddLiteral(tokenType, nil)
+}
+
+// isAtEnd returns if the scanner has reached the end of the current lexeme
+func (s *Scanner) isAtEnd() bool {
+	return s.current >= len(s.source)
+}
+
+// advance will move the current pointer to the next pointer in the source
+func (s *Scanner) advance() byte {
+	output := s.source[s.current]
+	s.current++
+
+	return output
+}
+
+// ScanToken scans an individual lexeme and matches it the token types
+func (s *Scanner) ScanToken() error {
+	c := s.advance() // move forward
+
+	switch c {
+	case '(':
+		fmt.Println("LEFT_PAREN ( null")
+		s.AddToken(LEFT_PAREN)
+	case ')':
+		fmt.Println("RIGHT_PAREN ) null")
+		s.AddToken(RIGHT_PAREN)
+	default:
+	}
+	s.AddToken(EOF)
+	return nil
+}
+
+// ScanTokens scans the tokens one by one
+func (s *Scanner) ScanTokens() ([]Token, error) {
+	// check if we have reached the end of the current lexeme
+	// if yes, set the start to the current token
+	isError := false
+	var err error
+	for !s.isAtEnd() {
+		s.start = s.current
+
+		err = s.ScanToken()
+		if err != nil {
+			isError = true
+		}
+	}
+
+	if isError && err != nil {
+		return s.tokens, errors.New(err.Error())
+	}
+
+	// append the EOF token for the end of the file
+	s.tokens = append(s.tokens, Token{
+		Type:    EOF,
+		Lexeme:  "",
+		Literal: nil,
+		Line:    s.line,
+	})
+
+	return s.tokens, nil
+}
+
+func main() {
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
 		os.Exit(1)
@@ -31,8 +136,8 @@ func main() {
 	}
 
 	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
-	} else {
-		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
+		s := NewScanner(fileContents)
+		_, _ = s.ScanTokens()
 	}
+	fmt.Println("EOF  null")
 }
